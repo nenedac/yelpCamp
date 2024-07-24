e59e92b6fe077d364b54e383b2bac4f2a4f8d1e2
Deyner Cruz <nenedac98@gmail.com>
2024-07-24 04:21:16 -0600

    replaces: "" for '' to enhance formatting across all documents

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const ejsMate = require("ejs-mate");
-const session = require("express-session");
-const flash = require("connect-flash");
-const ExpressError = require("./utils/ExpressError");
-const methodOverride = require("method-override");
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
 
 //MIDDLEWARE ROUTES
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 ///////////////////////////////////////////////
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
 });
 ///////////////////////////////////////////////
 
@@ -24,18 +24,18 @@ const app = express();
 
 ///////////////////////////////////////////////
 
-app.engine("ejs", ejsMate);
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 ///////////////////////////////////////////////
 
 const sessionConfig = {
-    secret: "thisshouldbeabettersecret!",
+    secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -48,31 +48,31 @@ app.use(session(sessionConfig));
 app.use(flash());
 
 app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
     next();
 });
 
 ///////////////////////////////////////////////
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
 
 //GET HOME PAGE
-app.get("/", (req, res) => {
-    res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.all("*", (req, res, next) => {
-    next(new ExpressError("Page Not Found", 404));
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 });
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (!err.message) err.message = "Oh No, Something Went Wrong!";
-    res.status(statusCode).render("error", { err });
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 ///////////////////////////////////////////////
 app.listen(3000, () => {
-    console.log("APP IS LISTENING ON PORT 3000!");
+    console.log('APP IS LISTENING ON PORT 3000!');
 });
--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 ///////////////////////////////////////////////
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
 });
 /////////////////////////////////////////////// 
 const sample = array => array[Math.floor(Math.random() * array.length)];
